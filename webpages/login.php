f4912cf6798650466647d4b11ae6c3393df10c20
shashwat40 <31835074+shashwat40@users.noreply.github.com>
2017-09-11 14:43:58 +0530

    Add files via upload

---
+++ webpages/login.php
@@ -0,0 +1,24 @@
+<?php
+
+$connection = mysqli_connect("localhost:3306","root","","central_railways_database");
+if(!$connection) {	
+	die('Could not connect : ' .msqli_error());
+}
+
+if(!empty($_POST['username'])) {
+	if(!empty($_POST['passwd'])) {
+		$username = $_POST['username'];
+		$password = $_POST['passwd'];
+		$login_query = "SELECT username, password FROM users WHERE username = '$username' AND password = '$password'";
+		$result = mysqli_query($connection, $login_query) or die(mysqli_error($connection));
+		if(mysqli_num_rows($result) === 0)
+			echo "Either Username or Password is incorrect.";
+		else
+			echo "Login successful.";
+	}
+	else
+		echo "Please enter password.";
+}
+else
+	echo "Please enter username.";
+?>
