2d95d95f8140fda75eda35be8d075206f845ee3f
AayushYadavv <31856483+AayushYadavv@users.noreply.github.com>
2017-09-28 23:35:47 +0530

    Update kkkosh.php

--- webpages/kkkosh.php
+++ webpages/kkkosh.php
@@ -4,33 +4,14 @@ $emp_name = "";
 $desig = "";
 $dept = "";
 $station = "";
-session_start();
 if(isset($_POST['getdetails'])) {
-    $_SESSION['empno'] = $_POST['empno'];
     $emp_no = $_POST['empno'];
-    $result = mysqli_query($connection, "SELECT emp_name, desig, dept, station FROM employees WHERE emp_no = '$emp_no'");
+    $result = mysqli_query($connection, "SELECT emp_name, desig, dept, station FROM employee WHERE emp_no = $emp_no");
     while($row = mysqli_fetch_assoc($result)) {
         $emp_name = $row['emp_name'];
         $desig = $row['desig'];
         $dept = $row['dept'];
         $station = $row['station'];
-    }
-}
-else if(isset($_POST['submitdetails'])) {
-    $emp_no = $_POST['empno'];
-    $cheque_no = $_POST['kkkoshchequeno'];
-    $cheque_date = $_POST['chequedate'];
-    $amount = $_POST['kkkoshamount'];
-    $paid_by = $_POST['kkkoshpaidby'];
-    $result = mysqli_query($connection, "INSERT INTO kkkosh (emp_no, kkkosh_cheq_no, kkkosh_cheq_date, kkkosh_cheq_amt, "
-                                        . "kkkosh_paid_by) VALUES ('$emp_no', '$cheque_no', '$cheque_date', $amount, '$paid_by')");
-    if(!$result) {
-        echo "<script> alert('Sorry! Data not inserted. Try again.'); </script>";
-    }
-    else {
-        echo "<script> alert('Data entered successfully.');</script>";
-        unset($_SESSION['empno']);
-        session_destroy();
     }
 }
 mysqli_close($connection);
@@ -39,14 +20,9 @@ mysqli_close($connection);
     <head>
         <title>Indian Railways</title>
             <link rel="stylesheet" href="user_dashboard.css">
-            <link rel="stylesheet" href="http://localhost/central_railways/plugins/jquery/jquery-ui.css">
-            <script src="http://localhost/central_railways/plugins/jquery/external/jquery/jquery.js"></script>
-            <script src="http://localhost/central_railways/plugins/jquery/jquery-ui.min.js"></script>
-            <script>
-                $(document).ready(function() {
-                    $("#chequedate").datepicker({ dateFormat : 'yy-mm-dd'});
-                });
-            </script>
+			<link rel="stylesheet" href="http://localhost/central_railways/bootstrap/css/bootstrap.min.css">
+<script src="http://localhost/central_railways/bootstrap/js/jquery-3.2.1.js"></script>
+<script src="http://localhost/central_railways/bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body>
 	<img align= left height= 120px src="http://localhost/central_railways/images/logo.png"></img>
@@ -61,15 +37,14 @@ mysqli_close($connection);
                 <li><a href="distress_fund.php">Distress Fund</a></li>
             </ul>
         </div>
-	<br>
-	<br>
+	<br><br>
         <form method="post" action="kkkosh.php">
             <fieldset>
                 <legend><h2>KKKOSH Details:</h2></legend>
                 <table align="left" border="0">
                     <tr>
                         <td width="165">Employee No. :</td>
-                        <td width="165"><input type="text" name="empno" id="empno" value="<?php echo isset($_SESSION['empno']) ? $_SESSION['empno'] : ''; ?>"></td>
+                        <td width="165"><input type="text" name="empno" id="empno"></td>
                     </tr>
                     <tr>
                         <td width="165">Name:</td>
@@ -94,7 +69,7 @@ mysqli_close($connection);
                         <td width="165"><input type="text" name="kkkoshchequeno" id="kkkoshchequeno"></td>
                     </tr>
                     <tr>
-                        <td width="165">KKKOSH Cheque Date :</td>
+                        <td width="165">KKKOSH ChequeDate :</td>
                         <td width="165"><input type="text" name="chequedate" id="chequedate"></td>
                     </tr>
                     <tr>
@@ -103,12 +78,10 @@ mysqli_close($connection);
                     </tr>
                     <tr>
                         <td width="165">KKKOSH Paid By :</td>
-                        <td width="165"><input type="text" name="kkkoshpaidby" id="kkkoshpaidby">
+                        <td width="165"><input type="text" name="kkkoshpaidto" id="kkkoshpaidto">
                     </tr>
                 </table>
-                <br><br>
                 <input type="submit" value="Get Details" name="getdetails">
-                <input type="submit" value="Submit" name="submitdetails">
             </fieldset>
         </form>
      </body>
