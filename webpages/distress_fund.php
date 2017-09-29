711555d46675f331fc4a1644c9ce6415b632ad39
Shashwat Amte <31835074+shashwat40@users.noreply.github.com>
2017-09-29 20:20:33 +0530

    Add files via upload

--- webpages/distress_fund.php
+++ webpages/distress_fund.php
@@ -46,6 +46,9 @@ mysqli_close($connection);
 			<link rel="stylesheet" href="http://localhost/central_railways/bootstrap/css/bootstrap.min.css">
 			<script src="http://localhost/central_railways/bootstrap/js/jquery-3.2.1.js"></script>
 			<script src="http://localhost/central_railways/bootstrap/js/bootstrap.min.js"></script>
+                        <link rel="stylesheet" href="http://localhost/central_railways/plugins/jquery/jquery-ui.css">
+        <script src="http://localhost/central_railways/plugins/jquery/external/jquery/jquery.js"></script>
+        <script src="http://localhost/central_railways/plugins/jquery/jquery-ui.min.js"></script>
 			<script>
                 $(document).ready(function() {
                     $("#chequedate").datepicker({ dateFormat : 'yy-mm-dd'});
